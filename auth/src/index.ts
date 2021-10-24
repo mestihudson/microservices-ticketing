cb090850014324f1a1e8c11c391a65cd5d5ea526
mestihudson <mestihudson@gmail.com>
2021-10-24 09:58:56 -0300

    refactor: move route handler to specific file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentRouter } from './routers/current';
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi, there!');
-});
+app.use(currentRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000!')
