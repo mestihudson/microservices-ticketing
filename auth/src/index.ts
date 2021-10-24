8bbf9c6fdc6a5a96c29bc81dbfe763c97e77948d
mestihudson <mestihudson@gmail.com>
2021-10-24 10:00:18 -0300

    feat: add signout route handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,12 +4,15 @@ import { json } from 'body-parser';
 import { currentRouter } from './routers/current';
 import { signinRouter } from './routers/signin';
 import { signupRouter } from './routers/signup';
+import { signoutRouter } from './routers/signout';
+
 const app = express();
 app.use(json());
 
 app.use(currentRouter);
 app.use(signinRouter);
 app.use(signupRouter);
+app.use(signoutRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000!')
