75f587429126a4f194b3480f321fa85b006941a1
mestihudson <mestihudson@gmail.com>
2021-10-24 09:59:35 -0300

    feat: add signin route handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentRouter } from './routers/current';
+import { signinRouter } from './routers/signin';
 const app = express();
 app.use(json());
 
 app.use(currentRouter);
+app.use(signinRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000!')
