3e73fc2b46bb0af2d01a69c52e04753cf2a7ba61
mestihudson <mestihudson@gmail.com>
2021-10-24 10:00:02 -0300

    feat: add signup route handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,11 +3,13 @@ import { json } from 'body-parser';
 
 import { currentRouter } from './routers/current';
 import { signinRouter } from './routers/signin';
+import { signupRouter } from './routers/signup';
 const app = express();
 app.use(json());
 
 app.use(currentRouter);
 app.use(signinRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000!')
