15cab88934c318e3c93df1e1fd987dea8a82f630
mestihudson <mestihudson@gmail.com>
2021-10-24 10:45:40 -0300

    feat: add basic validation to signup handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signup', (req, res) => {
-  res.send('Hi, there!');
+  const { email, password } = req.body;
+
+  if (!email || typeof email !== 'string') {
+    res.status(400).send('Provide a valid email');
+  }
+  if (!password || typeof password !== 'string') {
+    res.status(400).send('Provide a valid password');
+  }
 });
 
 export { router as signupRouter };
