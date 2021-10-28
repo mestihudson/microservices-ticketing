a044f27c7dffa83d8a65f6298abbe0bbf2e5335d
mestihudson <mestihudson@gmail.com>
2021-10-28 08:24:05 -0300

    feat: add throws to custom errors created

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+
 const router = express.Router();
 
 router.post('/api/users/signup', [
@@ -12,12 +15,11 @@ router.post('/api/users/signup', [
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).send(errors.array());
+    throw new RequestValidationError(errors.array());
   }
 
-  const { email, password } = req.body;
-
   console.log('Creating a user ...');
+  throw new DatabaseConnectionError();
 
   res.send({});
 });
