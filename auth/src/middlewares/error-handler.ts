90dc36c7fe42e49c647f1237a2c50812fec6a839
mestihudson <mestihudson@gmail.com>
2021-10-28 23:12:58 -0300

    feat: add custom formatted returns

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 export const errorHandler = (
   err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong', err);
 
-  res.status(400).send({
-    message: 'Something went wrong'
-  });
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [ { message: err.reason } ] });
+  }
+
+  res.status(400).send({ errors: [ { message: 'Something went wrong' } ] });
 };
