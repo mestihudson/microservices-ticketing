111a0d37b3eda42d4b51a035ba71996fadd1ad61
mestihudson <mestihudson@gmail.com>
2021-10-29 00:33:38 -0300

    feat: use async throw error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -16,8 +16,8 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
-app.all('*', () => {
-  throw new NotFoundError();
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
 });
 
 app.use(errorHandler);
