94c15194b57ba774ed13e15bfe36778325a4ad1b
mestihudson <mestihudson@gmail.com>
2021-11-01 17:51:35 -0300

    feat: add mongodb connection implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentRouter } from './routes/current';
 import { signinRouter } from './routes/signin';
@@ -23,6 +24,17 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on 3000!')
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on 3000!')
+  });
+};
+
+start();
