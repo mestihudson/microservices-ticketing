f6b19a3e621b0bb03d54eb0e7f9bcf9675932a97
mestihudson <mestihudson@gmail.com>
2021-11-02 20:49:23 -0300

    feat: use bad request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -23,8 +24,7 @@ router.post('/api/users/signup', [
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    console.log('Email in use');
-    return res.send({});
+    throw new BadRequestError('Email in use');
   }
 
   const user = User.build({ email, password });
