84314698e916b803d416d2915164cd5e3ea3849f
mestihudson <mestihudson@gmail.com>
2021-11-02 21:49:04 -0300

    feat: add pre-save mongoose hook to obfuscate password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 interface UserAttrs {
   email: string;
   password: string;
@@ -24,6 +26,15 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
