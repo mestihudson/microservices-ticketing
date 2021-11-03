c21d58e109865834d18ce7def3cdb3b06242f796
mestihudson <mestihudson@gmail.com>
2021-11-03 12:29:03 -0300

    refactor: move request validation to specific file

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -10,12 +10,7 @@ router.post('/api/users/signin', [
       .withMessage('Email must be valid'),
     body('password').trim().notEmpty()
       .withMessage('Password must be supplied')
-  ], (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
+  ], validateRequest, (req: Request, res: Response) => {
 });
 
 export { router as signinRouter };
