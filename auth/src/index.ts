961110f6ec9de648bc04b7887517496f840bec5d
mestihudson <mestihudson@gmail.com>
2021-11-03 00:09:26 -0300

    feat: assure JWT_KEY env var has been setted

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,6 +33,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be setted');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to mongodb');
