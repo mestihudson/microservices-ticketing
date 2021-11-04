c8b2988028d90112e59b32dfb31b4045ef8d2709
mestihudson <mestihudson@gmail.com>
2021-11-04 11:33:56 -0300

    feat: add current user service implementation

--- auth/src/routes/current.ts
+++ auth/src/routes/current.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/current', (req, res) => {
-  res.send('Hi, there!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentRouter };
