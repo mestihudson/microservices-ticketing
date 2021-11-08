303cf0177900efc55f07ba93e8318a7f3608f388
mestihudson <mestihudson@gmail.com>
2021-11-08 12:00:32 -0300

    feat: use not authorized error

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { NotAuthorizedError } from '../errors/not-authorized-error';
+
 export const requireAuth = (
   req: Request, res: Response, next: NextFunction) => {
   if (!req.currentUser) {
-    return res.status(401).send();
+    throw new NotAuthorizedError();
   }
+
+  next();
 };
