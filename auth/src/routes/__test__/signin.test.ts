d179cb2ba54659823041de499018d674adc4c4bd
mestihudson <mestihudson@gmail.com>
2021-11-09 15:41:57 -0300

    test: should responds with a cookie when given valid credentials

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -28,3 +28,18 @@ it('fails when an incorrect password is supplied', async () => {
     })
     .expect(400);
 });
+
+it('responds with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(200);
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
