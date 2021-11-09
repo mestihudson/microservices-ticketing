0653b6b9080a64ad6d4c11bf5968aaff51684d62
mestihudson <mestihudson@gmail.com>
2021-11-09 15:28:48 -0300

    test: should sets a cookie after successful signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -53,3 +53,11 @@ it('disallows duplicate emails', async () => {
     .send({ email: 'test@test.com', password: '12345' })
     .expect(400);
 });
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: '1234' })
+    .expect(201);
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
