fcad7ef37ef0af6d0826c16a0998b7730f13d718
mestihudson <mestihudson@gmail.com>
2021-11-11 03:39:14 -0300

    refactor: change some code formatting

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -5,20 +5,14 @@ import { app } from '../../app';
 it('fails when a email that does not exist is supplied', async () => {
   return request(app)
     .post('/api/users/signin')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
+    .send({ email: 'test@test.com', password: 'password' })
     .expect(400);
 });
 
 it('fails when an incorrect password is supplied', async () => {
   await request(app)
     .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
+    .send({ email: 'test@test.com', password: 'password' })
     .expect(201);
   await request(app)
     .post('/api/users/signin')
@@ -32,10 +26,7 @@ it('fails when an incorrect password is supplied', async () => {
 it('responds with a cookie when given valid credentials', async () => {
   await request(app)
     .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
+    .send({ email: 'test@test.com', password: 'password' })
     .expect(201);
   const response = await request(app)
     .post('/api/users/signin')
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -5,30 +5,21 @@ import { app } from '../../app';
 it('returns a 201 on successful signup', async () => {
   return request(app)
     .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
+    .send({ email: 'test@test.com', password: 'password' })
     .expect(201);
 });
 
 it('returns a 400 with an invalid email', async () => {
   return request(app)
     .post('/api/users/signup')
-    .send({
-      email: 'test',
-      password: 'password'
-    })
+    .send({ email: 'test', password: 'password' })
     .expect(400);
 });
 
 it('returns a 400 with an invalid password', async () => {
   return request(app)
     .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'p'
-    })
+    .send({ email: 'test@test.com', password: 'p' })
     .expect(400);
 });
 
