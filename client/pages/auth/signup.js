e51797c27be05062f9d9f4c8bd0f5dd06da931bf
mestihudson <mestihudson@gmail.com>
2021-11-12 17:19:40 -0300

    refactor: move service call to specific hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,19 @@
 import { useState } from 'react'
-import axios from 'axios'
+
+import useRequest from '../../hooks/use-request'
 
 const Signup = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState([])
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: { email, password }
+  })
 
   const onSubmit = async (e) => {
     e.preventDefault()
-
-    try {
-      const response = await axios
-        .post('/api/users/signup', { email, password })
-
-      console.log(response.data)
-    } catch(err) {
-      setErrors(err.response.data.errors)
-    }
+    await doRequest()
   }
 
   return (
@@ -39,16 +36,7 @@ const Signup = () => {
           onChange={e => setPassword(e.target.value)}
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-          <ul className="my-0">
-            {errors.map(error => (
-              <li key={error.message}>{error.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   )
