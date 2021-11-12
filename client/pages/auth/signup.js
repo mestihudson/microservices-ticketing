589396ad8b5271ee284f43ee4e085e56b83cb800
mestihudson <mestihudson@gmail.com>
2021-11-11 23:31:12 -0300

    feat: add state to controls

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <form>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control"/>
+        <input
+          className="form-control"
+          value={email}
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control"/>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+        />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
