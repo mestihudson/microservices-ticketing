de798d309e6cd62ca1278c468fed5706e07c6db6
mestihudson <mestihudson@gmail.com>
2021-11-11 23:31:37 -0300

    feat: add events to controls

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,14 +4,20 @@ const Signup = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
           className="form-control"
           value={email}
+          onChange={e => setEmail(e.target.value)}
         />
       </div>
       <div className="form-group">
@@ -20,6 +26,7 @@ const Signup = () => {
           type="password"
           className="form-control"
           value={password}
+          onChange={e => setPassword(e.target.value)}
         />
       </div>
       <button className="btn btn-primary">Sign Up</button>
