5babae71fc6db4a87a4d7d6d6f3a8fe3e9104144
mestihudson <mestihudson@gmail.com>
2021-11-12 21:30:30 -0300

    feat: add current service call

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const Landing = () => {
+import axios from 'axios'
+
+const Landing = ({ currentUser }) => {
+  console.log(currentUser)
   return <h1>Landing Page</h1>
+}
+
+Landing.getInitialProps = async () => {
+  const { data } = await axios.get('/api/users/current')
+  return data
 }
 
 export default Landing
