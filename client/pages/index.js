cfce839e5681866994abdb929dfae8f58ca4d72f
mestihudson <mestihudson@gmail.com>
2021-11-12 21:54:30 -0300

    fix: define host to ingress-nginx catches

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,11 @@ const Landing = ({ currentUser }) => {
 
 Landing.getInitialProps = async () => {
   if (typeof window === 'undefined') {
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current')
+    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current', {
+      headers: {
+        Host: 'ticketing.dev'
+      }
+    })
     return data
   } else {
     const { data } = await axios.get('/api/users/current')
