6ed4c8693127a9051eb10cef8288b74a2826d9b3
mestihudson <mestihudson@gmail.com>
2021-11-16 14:59:18 -0300

    test: should returns an error if an invalid title is provided

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -24,6 +24,23 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
 it('returns an error if an invalid price is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
+});
+
 it('create a ticket with valid inputs', async () => {});
