7b5270783c8c229c1b04fe7169ebe122ef59b5d1
mestihudson <mestihudson@gmail.com>
2021-11-16 15:03:22 -0300

    feat: add returns an error if an invalid title is provided

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
-import { requireAuth } from '@mestihudson-ticketing/common';
+import { requireAuth, validateRequest } from '@mestihudson-ticketing/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required')
+  ],
+  validateRequest, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
