8727851f2b5716479592a82acb5fe0506f729f6b
mestihudson <mestihudson@gmail.com>
2021-11-15 22:55:06 -0300

    chore: use env var added to tickets k8s deloyment file

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be setted');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be setted');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to mongodb');
   } catch (err) {
     console.error(err);
