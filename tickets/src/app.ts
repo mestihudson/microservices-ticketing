3ef0686370b2b755cd056e8830cdcdf70eda5866
mestihudson <mestihudson@gmail.com>
2021-11-15 23:25:29 -0300

    feat: add a route handler listening to /api/tickets for post requests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError } from '@mestihudson-ticketing/common';
+import { newTicketRouter } from '@/routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+
+app.use(newTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
