4259228387eaa7b0140b8ea462f5aeff1f6d8c95
mestihudson <mestihudson@gmail.com>
2021-11-16 19:14:38 -0300

    test: should returns a 404 if the ticket is not found

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -2,5 +2,12 @@ import request from 'supertest';
 
 import { app } from '@/app';
 
-it.todo('returns a 404 if the ticket is not found')
 it.todo('returns the ticket if it has been found')
+it('returns a 404 if the ticket is not found', async () => {
+	const id = 'ticket-id';
+	const response = await request(app)
+	  .get(`/api/tickets/${id}`)
+		.send()
+		.expect(404);
+});
+
