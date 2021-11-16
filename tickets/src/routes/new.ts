5668a0db1da3684db0a5f29972b3a25f1240c7a9
mestihudson <mestihudson@gmail.com>
2021-11-16 16:34:19 -0300

    feat: add create a ticket with valid inputs

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
 import { requireAuth, validateRequest } from '@mestihudson-ticketing/common';
+import { Ticket } from '@/models/ticket';
 
 const router = express.Router();
 
@@ -12,8 +13,13 @@ router.post(
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
   ],
-  validateRequest, (req: Request, res: Response) => {
-  res.sendStatus(200);
+  validateRequest, async (req: Request, res: Response) => {
+  const { title, price } = req.body;
+
+  const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+  await ticket.save()
+
+  res.status(201).send(ticket);
 });
 
 export { router as newTicketRouter };
