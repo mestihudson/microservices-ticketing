efa57d1c631daa9715bc0053047a97bc08ab35ef
mestihudson <mestihudson@gmail.com>
2021-11-16 15:05:02 -0300

    test: should returns an error if an invalid price is provided

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -24,7 +24,6 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid price is provided', async () => {});
 it('returns an error if an invalid title is provided', async () => {
   await request(app)
     .post('/api/tickets')
@@ -39,6 +38,32 @@ it('returns an error if an invalid title is provided', async () => {
     .set('Cookie', signin())
     .send({
       price: 10
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: 'dsfjapsodf',
+      price: -10
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: 'dsfjapsodf',
+      price: 0
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: 'dsfjapsodf'
     })
     .expect(400);
 });
