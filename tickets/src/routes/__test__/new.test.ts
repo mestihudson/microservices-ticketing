05888ced84fcf425a558d26e3baa636500f10f84
mestihudson <mestihudson@gmail.com>
2021-11-16 11:03:52 -0300

    test: should can only be accessed if the user is signed in

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,7 +9,13 @@ it('has a route handler listening to /api/tickets for post requests', async () =
   expect(response.status).not.toEqual(404);
 });
 
-it('can only be accessed if the user is signed in', async () => {});
+it('can only be accessed if the user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
+});
+
 it('returns an error if an invalid title is provided', async () => {});
 it('returns an error if an invalid price is provided', async () => {});
 it('create a ticket with valid inputs', async () => {});
