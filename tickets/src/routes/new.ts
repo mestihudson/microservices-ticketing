ffd2ef4ee3c128447b81f8bb993306753a900ad8
mestihudson <mestihudson@gmail.com>
2021-11-16 15:07:04 -0300

    feat: add returns an error if an invalid price is provided

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -9,7 +9,8 @@ router.post(
   '/api/tickets',
   requireAuth,
   [
-    body('title').not().isEmpty().withMessage('Title is required')
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
   ],
   validateRequest, (req: Request, res: Response) => {
   res.sendStatus(200);
