3404c47ff4f3b23c314c87cd49bd42261a76a172
mestihudson <mestihudson@gmail.com>
2021-11-17 14:53:00 -0300

    test: should returns a 401 if the user is not authenticated

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -12,7 +12,14 @@ it('retuns a 404 if the provided id does not exists', async () => {
 		.expect(404);
 });
 
-it.todo('returns a 401 if the user is not authenticated');
+it('returns a 401 if the user is not authenticated', async () => {
+	const id = new mongoose.Types.ObjectId().toHexString();
+	await request(app)
+	  .put(`/api/tickets/${id}`)
+		.send({ title: 'dklfjaçslkd', price: 20 })
+		.expect(401);
+});
+
 it.todo('returns a 401 if the user does not own the ticket');
 it.todo('returns a 400 if the user provides an invalid title or price');
 it.todo('updates the ticket provided valid inputs');
