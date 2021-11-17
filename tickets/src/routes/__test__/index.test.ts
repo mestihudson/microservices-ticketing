65c00be52aefbabf0045fc12a09361b46778c753
mestihudson <mestihudson@gmail.com>
2021-11-17 12:20:50 -0300

    test: should can fetch a list of tickets

--- tickets/src/routes/__test__/index.test.ts
+++ tickets/src/routes/__test__/index.test.ts
@@ -2,4 +2,23 @@ import request from 'supertest';
 
 import { app } from '@/app';
 
-it.todo('can fetch a list of tickets');
+const createTicket = () => {
+	return request(app)
+	  .post('/api/tickets')
+		.set('Cookie', signin())
+		.send({ title: 'çskdljfal', price: 10 })
+		.expect(201);
+};
+
+it('can fetch a list of tickets', async () => {
+	await createTicket();
+	await createTicket();
+	await createTicket();
+
+	const response = await request(app)
+	  .get('/api/tickets')
+		.send()
+		.expect(200);
+
+	expect(response.body.length).toEqual(3);
+});
