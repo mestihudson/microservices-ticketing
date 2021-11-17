2da3e029ce2059017df9d5c2850e98191ec14655
mestihudson <mestihudson@gmail.com>
2021-11-17 15:50:17 -0300

    feat: add returns a 400 if the user provides an invalid title or price

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 import { Ticket } from '@/models/ticket';
 import {
-  NotFoundError, requireAuth, NotAuthorizedError
+  NotFoundError, requireAuth, NotAuthorizedError, validateRequest
 } from '@mestihudson-ticketing/common';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
 
   if (!ticket) {
