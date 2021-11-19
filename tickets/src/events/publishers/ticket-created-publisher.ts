a61d73cca3c775ae94417ae41aacc4b74358e985
mestihudson <mestihudson@gmail.com>
2021-11-18 23:45:14 -0300

    feat: add ticket created publisher

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,7 @@
+import {
+	Publisher, Subjects, TicketCreatedEvent
+} from '@mestihudson-ticketing/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+}
