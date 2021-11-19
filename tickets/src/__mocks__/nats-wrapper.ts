f466b2eead472fe5fd2cd9c2203d0ef1be147784
mestihudson <mestihudson@gmail.com>
2021-11-19 14:38:00 -0300

    fix(test): change approach to observability

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,7 +1,8 @@
 export const natsWrapper = {
   client: {
-    publish: (subject: string, data: string, callback: () => void) => {
+    publish: jest.fn().mockImplementation((
+      subject: string, data: string, callback: () => void) => {
       callback();
-    }
+    })
   }
 };
