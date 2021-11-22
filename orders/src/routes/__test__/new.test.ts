b69fe7d7095dcad7971a9818ed35a58ee570b395
mestihudson <mestihudson@gmail.com>
2021-11-21 23:44:24 -0300

    test: add new order handler todo list

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+it.todo('should have a route handler listening to /api/orders for post requests');
+it.todo('should can only be accessed if the user is signed in');
+it.todo('should return a status other than 401 if the user is signed in');
+it.todo('should return an error if an titleId is not provided');
+it.todo('should return an error if the ticket does not exist');
+it.todo('should return an error if the ticket is already reserved');
+it.todo('should reserve the ticket');
+it.todo('should emit an event about created order');
