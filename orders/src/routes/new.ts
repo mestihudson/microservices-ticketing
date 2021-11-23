1cd1d02c43e27b69aa6f4e64f1ad25c093455cf9
mestihudson <mestihudson@gmail.com>
2021-11-23 00:12:42 -0300

    feat: add emit an event about created order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,10 @@ import {
 } from '@mestihudson-ticketing/common';
 import { Order } from '@/models/order';
 import { Ticket } from '@/models/ticket';
+import { natsWrapper } from '@/nats-wrapper';
+import {
+  OrderCreatedPublisher
+} from '@/events/publishers/order-created-publisher';
 
 const router = express.Router();
 
@@ -39,6 +43,17 @@ router.post(
     expiresAt: new Date()
   });
   await order.save();
+
+  new OrderCreatedPublisher(natsWrapper.client).publish({
+    id: order.id,
+    status: order.status,
+    userId: order.userId,
+    expiresAt: order.expiresAt.toISOString(),
+    ticket: {
+      id: ticket.id,
+      price: ticket.price
+    }
+  });
 
   res.status(201).send(order);
 });
