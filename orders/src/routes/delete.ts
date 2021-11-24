3a93d86eca5dbbceddd5d1c2b94d0e4f8a8fe3c7
mestihudson <mestihudson@gmail.com>
2021-11-24 18:46:35 -0300

    feat: add return 404 if the order does not exist

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 
-import { requireAuth } from '@mestihudson-ticketing/common';
+import { requireAuth, NotFoundError } from '@mestihudson-ticketing/common';
+import { Order } from '@/models/order';
 
 const router = express.Router();
 
@@ -8,6 +9,11 @@ router.delete(
   '/api/orders/:orderId',
   requireAuth,
   async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+  const order = await Order.findById(orderId);
+  if (!order) {
+    throw new NotFoundError();
+  }
   res.send({});
 });
 
