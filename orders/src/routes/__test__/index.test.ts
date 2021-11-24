a8ad40f8850f6441ce6d1a066d9319814e80c64a
mestihudson <mestihudson@gmail.com>
2021-11-23 23:59:17 -0300

    test: add index order handler todo list

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,4 @@
+it.todo('should have a route handler listening to /api/orders for get requests');
+it.todo('should can only be accessed if the user is signed in');
+it.todo('should return a status other than 401 if the user is signed in');
+it.todo('should fetch orders for an particular user');
