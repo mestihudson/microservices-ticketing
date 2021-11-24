b2f2f5ee5db972725a2905c92af37dcdb99b9fef
mestihudson <mestihudson@gmail.com>
2021-11-24 00:32:35 -0300

    feat: add fetch orders for an particular user

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express';
 
 import { requireAuth } from '@mestihudson-ticketing/common';
+import { Order } from '@/models/order';
 
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
-  res.send({});
+  const orders = await Order.find({
+    userId: req.currentUser!.id
+  });
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
