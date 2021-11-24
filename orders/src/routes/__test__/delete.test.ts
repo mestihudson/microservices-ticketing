84b771659a35a1ddb182ac251c00774185712bee
mestihudson <mestihudson@gmail.com>
2021-11-24 18:46:18 -0300

    test: should return 404 if the order does not exist

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -28,7 +28,15 @@ it('should return other than 401 if the user is signed in', async () => {
   expect(status).not.toBe(401);
 });
 
-it.todo('should return 404 if the order does not exist');
+it('should return 404 if the order does not exist', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const { status } = await request(app)
+    .delete(`/api/orders/${orderId}`)
+    .set('Cookie', signin())
+    .send({});
+  expect(status).toBe(404);
+});
+
 it.todo('should update order status to cancelled');
 it.todo('should return 401 if the order does not belong to user');
 it.todo('should emit a cancelled order event');
