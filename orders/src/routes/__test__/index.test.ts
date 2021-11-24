39cd627a7545f31aa8b850fe95c0bb6143775ac6
mestihudson <mestihudson@gmail.com>
2021-11-24 00:09:12 -0300

    test: should can only be accessed if the user is signed in

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -9,6 +9,12 @@ it('should have a route handler listening to /api/orders for get requests', asyn
   expect(status).not.toBe(404);
 });
 
-it.todo('should can only be accessed if the user is signed in');
+it('should can only be accessed if the user is signed in', async () => {
+  await request(app)
+    .get('/api/orders')
+    .send({})
+    .expect(401);
+});
+
 it.todo('should return a status other than 401 if the user is signed in');
 it.todo('should fetch orders for an particular user');
