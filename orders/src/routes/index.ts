64007bf564a28a5aba1246ededb17d5cdfba1748
mestihudson <mestihudson@gmail.com>
2021-11-24 00:09:40 -0300

    feat: add can only be accessed if the user is signed in
      feat: add return a status other than 401 if the user is signed in

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 
+import { requireAuth } from '@mestihudson-ticketing/common';
+
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
   res.send({});
 });
 
