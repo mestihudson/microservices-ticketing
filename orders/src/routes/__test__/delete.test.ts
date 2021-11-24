c134700019af1df6e07dd450c797ead266e83036
mestihudson <mestihudson@gmail.com>
2021-11-24 19:10:42 -0300

    test: should update order status to cancelled

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 
 import { app } from '@/app';
+import { Ticket } from '@/models/ticket';
+import { OrderStatus } from '@mestihudson-ticketing/common';
 
 it('should have a route handler listening to /api/orders/:orderId for delete requests', async () => {
   const orderId = new mongoose.Types.ObjectId().toHexString();
@@ -37,6 +39,23 @@ it('should return 404 if the order does not exist', async () => {
   expect(status).toBe(404);
 });
 
-it.todo('should update order status to cancelled');
+it('should update order status to cancelled', async () => {
+	const ticket = await Ticket.build({ title: 'concert', price: 20 });
+	ticket.save();
+
+	const { body: order } = await request(app)
+	  .post('/api/orders')
+		.set('Cookie', signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
+
+	const { body: updated } = await request(app)
+	  .delete(`/api/orders/${order.id}`)
+		.set('Cookie', signin())
+		.send({})
+		.expect(200);
+	expect(updated.status).toBe(OrderStatus.Cancelled);
+});
+
 it.todo('should return 401 if the order does not belong to user');
 it.todo('should emit a cancelled order event');
