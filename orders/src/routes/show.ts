e0b7a7882940fec3b0fb43919930b10952d0d8bc
mestihudson <mestihudson@gmail.com>
2021-11-24 16:39:53 -0300

    feat: add fetched order have be populated with the ticket

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -9,7 +9,7 @@ router.get(
   '/api/orders/:orderId',
   requireAuth,
   async (req: Request, res: Response) => {
-  const order = await Order.findById(req.params.orderId);
+  const order = await Order.findById(req.params.orderId).populate('ticket');
   if (!order) {
     throw new NotFoundError();
   }
