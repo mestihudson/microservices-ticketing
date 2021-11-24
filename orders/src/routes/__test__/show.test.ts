a3c0fa26174f7148de577b3867f88fe567e2966f
mestihudson <mestihudson@gmail.com>
2021-11-24 14:29:55 -0300

    test: add show order handler todo list

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,7 @@
+it.todo('should have a route handler listening to /api/orders/:orderId for get requests');
+describe('can only be accessed if the user is signed in', () => {
+  it.todo('should return 401 if it is not');
+  it.todo('should return other then 401 if it is');
+})
+it.todo('should fetch the orders');
+it.todo('should fetched order have be populated with the ticket');
