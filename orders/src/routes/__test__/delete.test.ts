5cd90b31d64b2db5d29e52b3f37a209d78eef681
mestihudson <mestihudson@gmail.com>
2021-11-24 17:20:54 -0300

    test: should have a route handler listening to /api/orders/:orderId for delete requests

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,4 +1,16 @@
-it.todo('should have a route handler listening to /api/orders/:orderId for delete requests');
+import request from 'supertest';
+import mongoose from 'mongoose';
+
+import { app } from '@/app';
+
+it('should have a route handler listening to /api/orders/:orderId for delete requests', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const { status } = await request(app)
+    .delete(`/api/orders/${orderId}`)
+    .send({});
+  expect(status).not.toBe(404);
+});
+
 it.todo('should return 401 if the user is not signed in');
 it.todo('should return other than 401 if the user is signed in');
 it.todo('should return 404 if the order does not exist');
