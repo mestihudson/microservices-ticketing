0df03a6dc3f8c859578a4a4a3a8cc411a9e92fb6
mestihudson <mestihudson@gmail.com>
2021-11-24 17:29:15 -0300

    test: should can only be accessed if the user is signed in

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -11,8 +11,23 @@ it('should have a route handler listening to /api/orders/:orderId for delete req
   expect(status).not.toBe(404);
 });
 
-it.todo('should return 401 if the user is not signed in');
-it.todo('should return other than 401 if the user is signed in');
+it('should return 401 if the user is not signed in', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .delete(`/api/orders/${orderId}`)
+    .send({})
+    .expect(401);
+});
+
+it('should return other than 401 if the user is signed in', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const { status } = await request(app)
+    .delete(`/api/orders/${orderId}`)
+    .set('Cookie', signin())
+    .send({});
+  expect(status).not.toBe(401);
+});
+
 it.todo('should return 404 if the order does not exist');
 it.todo('should update order status to cancelled');
 it.todo('should return 401 if the order does not belong to user');
