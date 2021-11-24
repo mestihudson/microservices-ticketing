db16e4ad67c05652ee9b04a38be23473b2da77c2
mestihudson <mestihudson@gmail.com>
2021-11-24 14:56:57 -0300

    test: should have a route handler listening to /api/orders/:orderId for get requests

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,4 +1,16 @@
-it.todo('should have a route handler listening to /api/orders/:orderId for get requests');
+import request from 'supertest';
+import mongoose from 'mongoose';
+
+import { app } from '@/app';
+
+it('should have a route handler listening to /api/orders/:orderId for get requests', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const { status } = await request(app)
+    .get(`/api/orders/${orderId}`)
+    .send({});
+  expect(status).not.toBe(404);
+});
+
 describe('can only be accessed if the user is signed in', () => {
   it.todo('should return 401 if it is not');
   it.todo('should return other then 401 if it is');
