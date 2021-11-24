414b1fb7c9863562132a86486f011f4a6f3f6d87
mestihudson <mestihudson@gmail.com>
2021-11-24 17:29:30 -0300

    feat: add can only be accessed if the user is signed in

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
 
+import { requireAuth } from '@mestihudson-ticketing/common';
+
 const router = express.Router();
 
 router.delete(
   '/api/orders/:orderId',
+  requireAuth,
   async (req: Request, res: Response) => {
   res.send({});
 });
