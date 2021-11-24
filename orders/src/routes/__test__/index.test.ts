626ae5ae9c8822f7f9da3cb9ea5cfa8b8f0e1255
mestihudson <mestihudson@gmail.com>
2021-11-24 00:04:33 -0300

    test: should have a route handler listening to /api/orders for get requests

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,4 +1,14 @@
-it.todo('should have a route handler listening to /api/orders for get requests');
+import request from 'supertest';
+
+import { app } from '@/app';
+
+it('should have a route handler listening to /api/orders for get requests', async () => {
+  const { status } = await request(app)
+    .get('/api/orders')
+    .send({});
+  expect(status).not.toBe(404);
+});
+
 it.todo('should can only be accessed if the user is signed in');
 it.todo('should return a status other than 401 if the user is signed in');
 it.todo('should fetch orders for an particular user');
