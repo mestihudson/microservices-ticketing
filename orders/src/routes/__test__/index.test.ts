a21f76c27b11a96643b8b4aaabc7567081388be5
mestihudson <mestihudson@gmail.com>
2021-11-24 00:32:15 -0300

    test: should fetch orders for an particular user

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 
 import { app } from '@/app';
+import { Order } from '@/models/order';
+import { Ticket } from '@/models/ticket';
 
 it('should have a route handler listening to /api/orders for get requests', async () => {
   const { status } = await request(app)
@@ -23,4 +25,43 @@ it('should return a status other than 401 if the user is signed in', async () =>
     .send({});
   expect(status).not.toBe(401);
 });
-it.todo('should fetch orders for an particular user');
+
+const buildTicket = async () => {
+	const ticket = Ticket.build({ title: 'concert', price: 20	});
+	await ticket.save();
+	return ticket
+};
+
+const buildOrder = async (user: any, ticket: any) => {
+	await request(app)
+	  .post('/api/orders')
+		.set('Cookie', user)
+		.send({ ticketId: ticket.id })
+		.expect(201);
+};
+
+it('should fetch orders for an particular user', async () => {
+	const ticketOne = await buildTicket();
+	const ticketTwo = await buildTicket();
+	const ticketThree = await buildTicket();
+
+	const userOne = signin('1@1.com', '1');
+	const userTwo = signin('2@2.com', '2');
+
+	await buildOrder(userOne, ticketOne);
+	await buildOrder(userTwo, ticketTwo);
+	await buildOrder(userTwo, ticketThree);
+
+	const responseOne = await request(app)
+	  .get('/api/orders')
+		.set('Cookie', userOne)
+		.send({})
+		.expect(200);
+	expect(responseOne.body.length).toBe(1);
+	const responseTwo = await request(app)
+	  .get('/api/orders')
+		.set('Cookie', userTwo)
+		.send({})
+		.expect(200);
+	expect(responseTwo.body.length).toBe(2);
+});
