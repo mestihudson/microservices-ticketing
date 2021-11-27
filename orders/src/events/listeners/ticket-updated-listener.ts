14353d1a192564dfecc56b61a17c059783264955
mestihudson <mestihudson@gmail.com>
2021-11-26 23:52:16 -0300

    feat: change query to find ticket at db

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -11,8 +11,11 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent['data'], message: Message) {
-    const { id, title, price } = data;
-    const ticket = await Ticket.findById(id);
+    const { id, title, price, version } = data;
+    const ticket = await Ticket.findOne({
+      _id: id,
+      version: version - 1
+    });
 
     if (!ticket) {
       throw new Error('Ticket not found');
