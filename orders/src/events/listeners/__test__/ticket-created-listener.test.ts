ed9eed57175fd00162cc610b54be8e73abfd5f30
mestihudson <mestihudson@gmail.com>
2021-11-27 01:12:01 -0300

    test: should create and save a ticket

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -1,2 +1,40 @@
-it.todo('should create and save a ticket');
+import { Message } from 'node-nats-streaming';
+import mongoose from 'mongoose';
+
+import { TicketCreatedEvent } from '@mestihudson-ticketing/common';
+import {
+  TicketCreatedListener
+} from '@/events/listeners/ticket-created-listener';
+import { natsWrapper } from '@/nats-wrapper';
+import { Ticket } from '@/models/ticket';
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client);
+  const data: TicketCreatedEvent['data'] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 10,
+    userId: new mongoose.Types.ObjectId().toHexString()
+  };
+
+  // @ts-ignore
+  const message: Message = {
+  };
+
+  return { listener, data, message };
+};
+
+it('should create and save a ticket', async () => {
+  const { listener, data, message } = await setup();
+
+  await listener.onMessage(data, message);
+
+  const ticket = await Ticket.findById(data.id);
+  expect(ticket).toMatchObject(expect.objectContaining({
+    title: data.title,
+    price: data.price
+  }));
+});
+
 it.todo('should ack the message');
