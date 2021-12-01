261c49504696caf3636a09e58ce9a15eadfe6310
mestihudson <mestihudson@gmail.com>
2021-12-01 01:03:15 -0300

    test: should throw an error if ticket has not found

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,3 +1,39 @@
-it.todo('should throw an error if ticket has not found');
 it.todo('should update ticket with orderId that own request')
 it.todo('should acknowledge message')
+import { Message } from 'node-nats-streaming';
+
+import {
+  OrderCreatedListener
+} from '@/events/listeners/order-created-listener';
+import { natsWrapper } from '@/nats-wrapper';
+import { OrderCreatedEvent, OrderStatus } from '@mestihudson-ticketing/common';
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent['data'] = {
+    id: 'order-id',
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'user-id',
+    expiresAt: 'expiresAt',
+    ticket: { id: 'ticket-id', price: 1 }
+  };
+  //@ts-ignore
+  const message: Message = {
+  };
+  return { listener, data, message };
+};
+
+it('should throw an error if ticket has not found', async () => {
+  const { listener, data, message } = await setup();
+
+  try {
+    await listener.onMessage(data, message);
+  } catch (error) {
+    return;
+  }
+
+  throw new Error('An exception should have been raised');
+});
+
