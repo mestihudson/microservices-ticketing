a0c0ab6332f66c47c69f6f135d35321bb229bbd1
mestihudson <mestihudson@gmail.com>
2021-12-01 01:04:03 -0300

    feat: add throw an error if ticket has not found

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,19 @@
+import { Message } from 'node-nats-streaming';
+
+import {
+  Subjects, Listener, OrderCreatedEvent
+} from '@mestihudson-ticketing/common';
+import { Ticket } from '@/models/ticket';
+import { queueGroupName } from '@/events/listeners/queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], message: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+  }
+}
