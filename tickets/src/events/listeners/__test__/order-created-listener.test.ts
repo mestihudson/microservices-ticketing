4187d70aefddd0e1d331638f1e41c2ad4f005570
mestihudson <mestihudson@gmail.com>
2021-12-01 01:06:52 -0300

    test: should update ticket with orderId that own request

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,23 +1,26 @@
-it.todo('should update ticket with orderId that own request')
 it.todo('should acknowledge message')
 import { Message } from 'node-nats-streaming';
+import mongoose from 'mongoose';
 
 import {
   OrderCreatedListener
 } from '@/events/listeners/order-created-listener';
 import { natsWrapper } from '@/nats-wrapper';
 import { OrderCreatedEvent, OrderStatus } from '@mestihudson-ticketing/common';
+import { Ticket } from '@/models/ticket';
 
-const setup = async () => {
+const setup = async (orderId: string = 'order-id', ticket?: any) => {
   const listener = new OrderCreatedListener(natsWrapper.client);
 
+  const { id, price } = ticket || { id: 'ticket-id', price: 1 };
+
   const data: OrderCreatedEvent['data'] = {
-    id: 'order-id',
+    id: orderId,
     version: 0,
     status: OrderStatus.Created,
     userId: 'user-id',
     expiresAt: 'expiresAt',
-    ticket: { id: 'ticket-id', price: 1 }
+    ticket: { id, price }
   };
   //@ts-ignore
   const message: Message = {
@@ -35,5 +38,23 @@ it('should throw an error if ticket has not found', async () => {
   }
 
   throw new Error('An exception should have been raised');
+});
+
+it('should update ticket with orderId that own request', async () => {
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+    userId: 'user-id'
+  });
+  await ticket.save();
+
+  const { listener, data, message } = await setup(orderId, ticket);
+
+  await listener.onMessage(data, message);
+
+  const updated = await Ticket.findById(ticket.id);
+  expect(updated!.orderId).toBe(orderId);
 });
 
