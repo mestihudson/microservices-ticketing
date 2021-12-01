edb1b575f02e6f0ace3e76e20b26ac42f67c4733
mestihudson <mestihudson@gmail.com>
2021-12-01 16:47:13 -0300

    test: add order cancelled listener todo list

---
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,4 @@
+it.todo("should throw an error if ticket has not found");
+it.todo("should update orderId field of ticket with undefined value");
+it.todo("should acknowledge message");
+it.todo("should notify about ticket update");
