16e23221529989749e603c40928a6f5d183004dc
mestihudson <mestihudson@gmail.com>
2021-12-06 14:51:52 -0300

    feat: add throw an error if ticket has not found

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,19 @@
+import {
+  Subjects,
+  Listener,
+  OrderCancelledEvent,
+} from "@mestihudson-ticketing/common";
+import { queueGroupName } from "@/events/listeners/queue-group-name";
+import { Ticket } from "@/models/ticket";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"]) {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+  }
+}
