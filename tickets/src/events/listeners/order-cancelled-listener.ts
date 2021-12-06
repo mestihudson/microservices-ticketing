3515f2661eebf0625657aab4c54d946889c63ad0
mestihudson <mestihudson@gmail.com>
2021-12-06 16:32:29 -0300

    feat: add notify about ticket update

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -7,6 +7,8 @@ import {
 } from "@mestihudson-ticketing/common";
 import { queueGroupName } from "@/events/listeners/queue-group-name";
 import { Ticket } from "@/models/ticket";
+import { TicketUpdatedPublisher } from "@/events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "@/nats-wrapper";
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   readonly subject = Subjects.OrderCancelled;
@@ -19,6 +21,14 @@ export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
     }
     ticket.set({ orderId: undefined });
     await ticket.save();
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
     message.ack();
   }
 }
