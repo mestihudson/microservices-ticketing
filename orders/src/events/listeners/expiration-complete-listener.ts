65aed322a2d64b80c22c1ff9739bd5c959e7d9d5
mestihudson <mestihudson@gmail.com>
2021-12-07 00:35:21 -0300

    feat: add raise an error if order has not found

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -6,10 +6,16 @@ import {
   ExpirationCompleteEvent,
 } from "@mestihudson-ticketing/common";
 import { queueGroupName } from "@/events/listeners/queue-group-name";
+import { Order } from "@/models/order";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
   readonly queueGroupName = queueGroupName;
 
-  async onMessage(data: ExpirationCompleteEvent["data"], message: Message) {}
+  async onMessage(data: ExpirationCompleteEvent["data"], message: Message) {
+    const order = await Order.findById(data.orderId);
+    if (!order) {
+      throw new Error("Order has not found");
+    }
+  }
 }
