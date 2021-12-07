b8a7b3edb1b6e319aa79064d47b11c47469853b4
mestihudson <mestihudson@gmail.com>
2021-12-07 02:12:02 -0300

    chore: customize src/index.ts file

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,17 +1,6 @@
-import mongoose from "mongoose";
-
-import { app } from "@/app";
 import { natsWrapper } from "@/nats-wrapper";
-import { OrderCreatedListener } from "@/events/listeners/order-created-listener";
-import { OrderCancelledListener } from "@/events/listeners/order-cancelled-listener";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be setted");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be setted");
-  }
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error("NATS_CLUSTER_ID must be setted");
   }
@@ -34,19 +23,9 @@ const start = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to mongodb");
   } catch (err) {
     console.error(err);
   }
-
-  app.listen(3000, () => {
-    console.log("Listening on 3000!");
-  });
 };
 
 start();
