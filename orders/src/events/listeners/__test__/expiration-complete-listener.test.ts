57951eddf9e56d22f4e0400b6b9a8ae997b5be29
mestihudson <mestihudson@gmail.com>
2021-12-07 01:08:36 -0300

    test: should update order status to cancelled

--- orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -1,10 +1,14 @@
+import {
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from "@mestihudson-ticketing/common";
 import mongoose from "mongoose";
 import { Message } from "node-nats-streaming";
 
-import { ExpirationCompleteEvent } from "@mestihudson-ticketing/common";
 import { ExpirationCompleteListener } from "@/events/listeners/expiration-complete-listener";
 import { natsWrapper } from "@/nats-wrapper";
 import { Order } from "@/models/order";
+import { Ticket } from "@/models/ticket";
 
 it("should raise an error if order has not found", async () => {
   const listener = new ExpirationCompleteListener(natsWrapper.client);
@@ -24,6 +28,36 @@ it("should raise an error if order has not found", async () => {
   throw new Error("An exception should have been raised");
 });
 
-it.todo("should update order status to cancelled");
+it("should update order status to cancelled", async () => {
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "user-id",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const message: Message = {};
+
+  await listener.onMessage(data, message);
+
+  const expiredOrder = await Order.findById(order.id);
+  expect(expiredOrder!.status).toBe(OrderStatus.Cancelled);
+});
+
 it.todo("should emit an order cancelled event");
 it.todo("should acknowledge message");
