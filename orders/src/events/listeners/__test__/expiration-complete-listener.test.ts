0e4014f06ebdb14477a0031be0c84143bf05cdcb
mestihudson <mestihudson@gmail.com>
2021-12-06 23:04:09 -0300

    test: add expiration complete listener todo list

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,4 @@
+it.todo("should raise an error if order has not found");
+it.todo("should update order status to cancelled");
+it.todo("should emit an order cancelled event");
+it.todo("should acknowledge message");
