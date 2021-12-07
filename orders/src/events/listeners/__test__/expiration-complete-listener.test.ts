a5e1421fe0c83865fe69c7502fa515d859fce142
mestihudson <mestihudson@gmail.com>
2021-12-07 01:31:57 -0300

    test: should emit an order cancelled event

--- orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -59,5 +59,45 @@ it("should update order status to cancelled", async () => {
   expect(expiredOrder!.status).toBe(OrderStatus.Cancelled);
 });
 
-it.todo("should emit an order cancelled event");
+it("should emit an order cancelled event", async () => {
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "user-id",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const message: Message = {};
+
+  await listener.onMessage(data, message);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalledTimes(1);
+  expect(natsWrapper.client.publish).toHaveBeenNthCalledWith(
+    1,
+    "order:cancelled",
+    expect.stringMatching(/ticket/),
+    expect.any(Function)
+  );
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+  expect(eventData.id).toBe(order.id);
+  expect(eventData.ticket.id).toBe(ticket.id);
+});
+
 it.todo("should acknowledge message");
