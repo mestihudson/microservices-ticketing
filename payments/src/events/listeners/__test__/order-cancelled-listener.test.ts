8c1c341b10a00703ea1b5f8f93382ed786d8f734
mestihudson <mestihudson@gmail.com>
2021-12-08 03:12:28 -0300

    test: should throw an error if order has not found

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,4 +1,17 @@
-it.todo("should throw an error if order has not found");
+it("should throw an error if order has not found", async () => {
+  await createOrder();
+
+  const { listener, data, message } = await setup();
+
+  try {
+    await listener.onMessage(data, message);
+  } catch (err) {
+    return;
+  }
+
+  throw new Error("should have been raised an error");
+});
+
 it.todo("should throw an error if order version is not immediately ancestry");
 it.todo("should change order status to cancelled");
 it.todo("should acknowlegde message");
