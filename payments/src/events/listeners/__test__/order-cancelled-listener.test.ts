781ed7ff8556dbed6c9060d7e3c0feb229a41280
mestihudson <mestihudson@gmail.com>
2021-12-08 03:26:17 -0300

    test(precompile): add helper functions

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,3 +1,42 @@
+import mongoose from "mongoose";
+
+import {
+  OrderStatus,
+  OrderCancelledEvent,
+} from "@mestihudson-ticketing/common";
+
+import { Order } from "@/models/order";
+import { OrderCancelledListener } from "@/events/listeners/order-cancelled-listener";
+import { natsWrapper } from "@/nats-wrapper";
+
+const createOrder = async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 20,
+    status: OrderStatus.Created,
+    userId: "userid",
+    version: 0,
+  });
+  await order.save();
+};
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const data: OrderCancelledEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    ticket: {
+      id: "ticketId",
+    },
+  };
+
+  // @ts-ignore
+  const message: Message = {};
+
+  return { listener, data, message };
+};
+
 it("should throw an error if order has not found", async () => {
   await createOrder();
 
