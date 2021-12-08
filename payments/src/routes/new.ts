90823f407f01a1566d27245867eb81d929ef2397
mestihudson <mestihudson@gmail.com>
2021-12-08 04:52:32 -0300

    feat: add permit access only to signed in users

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 
+import { requireAuth } from "@mestihudson-ticketing/common";
+
 const router = express.Router();
 
-router.post("/api/payments", (req: Request, res: Response) => {
+router.post("/api/payments", requireAuth, (req: Request, res: Response) => {
   res.send({});
 });
 
