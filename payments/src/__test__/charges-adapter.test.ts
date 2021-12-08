f034146e1e3f9e795bfd0fdc7b3ca855d361f982
mestihudson <mestihudson@gmail.com>
2021-12-08 20:44:24 -0300

    test: should create a charge on stripe service

--- payments/src/__test__/charges-adapter.test.ts
+++ payments/src/__test__/charges-adapter.test.ts
@@ -1,1 +1,10 @@
-it.todo("should create a charge on stripe service");
+it("should create a charge on stripe service", async () => {
+  const value = Math.random();
+  const price = value * 100;
+  const token = "tok_visa";
+
+  await chargesAdapter.create(price, token);
+
+  const { charge: after } = await getLatestCharge(price);
+  expect(after).toBeDefined();
+});
