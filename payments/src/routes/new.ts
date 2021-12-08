17a4c39c7e5d9dc192c0df197093405b9e671a67
mestihudson <mestihudson@gmail.com>
2021-12-08 05:02:37 -0300

    feat: add throw an error if token has not provided

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
-import { requireAuth } from "@mestihudson-ticketing/common";
+import { requireAuth, validateRequest } from "@mestihudson-ticketing/common";
 
 const router = express.Router();
 
-router.post("/api/payments", requireAuth, (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/payments",
+  requireAuth,
+  body("token").not().isEmpty(),
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newPaymentRouter };
