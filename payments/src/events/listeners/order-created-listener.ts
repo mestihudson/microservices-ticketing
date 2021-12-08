3882d09b322ea6933649678a074f54eddc47ff96
mestihudson <mestihudson@gmail.com>
2021-12-08 02:48:05 -0300

    feat: add create an order

--- payments/src/events/listeners/order-created-listener.ts
+++ payments/src/events/listeners/order-created-listener.ts
@@ -6,9 +6,20 @@ import {
   OrderCreatedEvent,
 } from "@mestihudson-ticketing/common";
 
+import { Order } from "@/models/order";
+
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   readonly queueGroupName = "payments-service";
 
-  async onMessage(data: OrderCreatedEvent["data"], message: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], message: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+  }
 }
