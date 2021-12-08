6a562346a2b8d55b51bdf499c4896bb60d9caa63
mestihudson <mestihudson@gmail.com>
2021-12-08 04:38:35 -0300

    test(todo): add new route handler

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,7 @@
+it.todo("should handle post requests by listening to /api/payments route");
+it.todo("should permit access only to signed in users");
+it.todo("should throw an error if token has not provided");
+it.todo("should throw an error if orderId has not provided");
+it.todo("should throw an error if order has not found");
+it.todo("should throw an error if order has not belong to current user");
+it.todo("should throw an error if order has cancelled");
