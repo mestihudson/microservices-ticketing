88b7a4ac5c4814d5a5c3341efd3d83cfbea4aede
mestihudson <mestihudson@gmail.com>
2021-12-08 03:01:58 -0300

    test(todo): add order cancelled listener

---
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,4 @@
+it.todo("should throw an error if order has not found");
+it.todo("should throw an error if order version is not immediately ancestry");
+it.todo("should change order status to cancelled");
+it.todo("should acknowlegde message");
