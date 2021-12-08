ed5dafb15bf7ba4b429b08690b31a243de529a3e
mestihudson <mestihudson@gmail.com>
2021-12-08 01:53:16 -0300

    test: should create an order

--- payments/src/events/listeners/__test__/order-created-listener.test.ts
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,2 +1,10 @@
-it.todo("should create an order");
+it("should create an order", async () => {
+  const { listener, data, message, orderId } = await setup();
+
+  await listener.onMessage(data, message);
+
+  const order = await Order.findById(orderId);
+  expect(order).not.toBeNull();
+});
+
 it.todo("should acknowlegde message");
