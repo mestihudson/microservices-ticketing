2ec3c74f0146bfa7ae0690fcf10faa00f935ab35
mestihudson <mestihudson@gmail.com>
2021-12-08 21:13:15 -0300

    feat: add create a charge on stripe service

--- payments/src/charges-adapter.ts
+++ payments/src/charges-adapter.ts
@@ -3,7 +3,16 @@ import { randomBytes } from "crypto";
 import { stripe } from "@/stripe";
 
 class ChargesAdapter {
-  async create(price: number, token: string) {}
+  async create(price: number, source: string) {
+    const description = randomBytes(12).toString("hex");
+    const amount = Math.floor(price * 100);
+    await stripe.charges.create({
+      amount,
+      source,
+      currency: "usd",
+      description,
+    });
+  }
 }
 
 export const chargesAdapter = new ChargesAdapter();
