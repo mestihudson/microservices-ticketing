75503d3fb6bc642afb86a20294bd400a3a6efd15
mestihudson <mestihudson@gmail.com>
2021-12-10 01:05:10 -0300

    feat: add return a object with stripeId to created charge on stripe service

--- payments/src/charges-adapter.ts
+++ payments/src/charges-adapter.ts
@@ -3,15 +3,16 @@ import { randomBytes } from "crypto";
 import { stripe } from "@/stripe";
 
 class ChargesAdapter {
-  async create(price: number, source: string) {
+  async create(price: number, source: string): Promise<any> {
     const description = randomBytes(12).toString("hex");
     const amount = Math.floor(price * 100);
-    await stripe.charges.create({
+    const { id: stripeId } = await stripe.charges.create({
       amount,
       source,
       currency: "usd",
       description,
     });
+    return new Promise((resolve) => resolve({ stripeId }));
   }
 }
 
