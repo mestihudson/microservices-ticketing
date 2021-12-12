19fe7fb6ff07868f410dfb9ec28cbe431eb94a97
mestihudson <mestihudson@gmail.com>
2021-12-11 22:08:43 -0300

    test(todo): add payment created listener

---
+++ orders/src/events/listeners/__test__/payment-created-listener.test.ts
@@ -0,0 +1,4 @@
+it.todo("should throw an error if order has not found");
+it.todo("should update order status to completed");
+it.todo("should emit a order completed event");
+it.todo("should acknowledge a message");
