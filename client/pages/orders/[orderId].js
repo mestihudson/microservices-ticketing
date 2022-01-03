c65ef4e5c98ccc0c7f72a2ba08dc4d610603af0f
mestihudson <mestihudson@gmail.com>
2022-01-03 17:33:42 -0300

    feat: add seconds to expires order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,15 @@
-const OrderShow = () => {
+const OrderShow = ({ order }) => {
+  const msLeft = new Date(order.expiresAt) - new Date()
   return (
-    <div>OrderShow</div>
+    <div>{msLeft / 1000} seconds until order expires</div>
   )
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query
+  const { data } = await client.get(`/api/orders/${orderId}`)
+
+  return { order: data }
 }
 
 export default OrderShow
