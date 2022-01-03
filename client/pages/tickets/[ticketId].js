f391f9537d8120b58e74747f264f95b641d9c69e
mestihudson <mestihudson@gmail.com>
2022-01-03 16:56:33 -0300

    feat: add ticket details to show page

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,7 +1,17 @@
-const TicketShow = () => {
+const TicketShow = ({ ticket }) => {
   return (
-    <div>TicketShow</div>
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+    </div>
   )
+}
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query
+  const { data } = await client.get(`/api/tickets/${ticketId}`)
+
+  return { ticket: data }
 }
 
 export default TicketShow
