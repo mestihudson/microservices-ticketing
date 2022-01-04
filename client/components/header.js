fef4b9ddf53ffff5cdbcf8b6189df942f25b7ee0
mestihudson <mestihudson@gmail.com>
2022-01-04 01:32:41 -0300

    feat: add link to new tickets on header

--- client/components/header.js
+++ client/components/header.js
@@ -4,6 +4,7 @@ const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' }
   ]
     .filter(config => config)
