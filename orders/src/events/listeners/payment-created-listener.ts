2a15db2c72d2f046d5a49362efab727154ba0b96
mestihudson <mestihudson@gmail.com>
2022-01-03 23:11:54 -0300

    feat: add throw an error if order has not found

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -6,11 +6,16 @@ import {
   PaymentCreatedEvent,
 } from "@mestihudson-ticketing/common";
 import { queueGroupName } from "@/events/listeners/queue-group-name";
+import { Order } from "@/models/order";
 
 export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
   readonly subject = Subjects.PaymentCreated;
   readonly queueGroupName = queueGroupName;
 
   async onMessage(data: PaymentCreatedEvent["data"], message: Message) {
+    const order = await Order.findById(data.orderId);
+    if (!order) {
+      throw new Error("Order not found");
+    }
   }
 }
