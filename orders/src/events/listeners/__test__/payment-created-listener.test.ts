0010f70f1438ba34e30c365dbfa9f20d0842c853
mestihudson <mestihudson@gmail.com>
2022-01-03 23:04:00 -0300

    test: should throw an error if order has not found

--- orders/src/events/listeners/__test__/payment-created-listener.test.ts
+++ orders/src/events/listeners/__test__/payment-created-listener.test.ts
@@ -1,4 +1,15 @@
-it.todo("should throw an error if order has not found");
+it("should throw an error if order has not found", async () => {
+  const { listener, data, message } = await setup();
+
+  try {
+    await listener.onMessage(data, message);
+  } catch (error) {
+    return;
+  }
+
+  throw new Error("An exception should have been raised");
+});
+
 it.todo("should update order status to completed");
 it.todo("should emit a order completed event");
 it.todo("should acknowledge a message");
